83e03edfd77d3f6aea1151deb2ab440d9cfe8bfe
DanielTakac <danieltakac@protonmail.com>
2022-12-03 14:50:39 +0100

    Added day3 project

---
+++ 2022/day3/day3.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day1 {
+
+    class Day1 {
+
+        static void Main(string[] args) {
+
+            
+
+        }
+
+    }
+
+}
