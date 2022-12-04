11437f6de19bfbe10345fe6e91022f2bb02982b6
DanielTakac <danieltakac@protonmail.com>
2022-12-04 20:55:11 +0100

    Finished part 1 of day 4

--- 2022/day4/day4.cs
+++ 2022/day4/day4.cs
@@ -14,11 +14,36 @@ namespace day4 {
 
             string[] input = File.ReadAllLines(path);
 
+            int part1 = 0;
+
             foreach (string line in input) {
 
+                int[] firstElfSections = new int[2];
+                int[] secondElfSections = new int[2];
 
+                string[] splitLine = line.Split(',');
+
+                (firstElfSections[0], firstElfSections[1]) = (int.Parse(splitLine[0].Split('-')[0]), int.Parse(splitLine[0].Split('-')[1]));
+                (secondElfSections[0], secondElfSections[1]) = (int.Parse(splitLine[1].Split('-')[0]), int.Parse(splitLine[1].Split('-')[1]));
+
+                Console.ResetColor();
+
+                if ((firstElfSections[0] <= secondElfSections[0] && firstElfSections[1] >= secondElfSections[1]) ||
+                    (firstElfSections[0] >= secondElfSections[0] && firstElfSections[1] <= secondElfSections[1])) {
+
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+
+                    part1++;
+
+                }
+
+                Console.WriteLine($"\nFirst Elf: {firstElfSections[0]}-{firstElfSections[1]}\nSecond Elf: {secondElfSections[0]}-{secondElfSections[1]}\n");
 
             }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"\nPart 1: {part1}");
+            Console.ResetColor();
 
             Console.ReadKey();
 
