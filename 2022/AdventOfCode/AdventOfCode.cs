d99109d21ee3d53b70b27f5b21f0744bb43f8402
DanielTakac <danieltakac@protonmail.com>
2022-12-05 22:51:21 +0100

    Added a few helper functions

--- 2022/AdventOfCode/AdventOfCode.cs
+++ 2022/AdventOfCode/AdventOfCode.cs
@@ -8,9 +8,39 @@ namespace AdventOfCode {
     
     public class AdventOfCode {
 
-        public static string[] GetInput(bool demo = false, string path = "input.txt") {
+        public static string[] GetInput(string path = "input.txt") => File.ReadAllLines(path);
 
-            return File.ReadAllLines(path);
+        public static int GetInputLineCount(string path = "input.txt") => GetInput(path).Count();
+
+        public static int GetCharacterCount(string path = "input.txt") {
+
+            string[] input = GetInput(path);
+
+            int characterCount = 0;
+
+            foreach (string line in input) {
+
+                characterCount += line.Length;
+
+            }
+
+            return characterCount;
+
+        }
+
+        public static int GetCharacterCount(char character, string path = "input.txt") {
+
+            string[] input = GetInput(path);
+
+            int characterCount = 0;
+
+            foreach (string line in input) {
+
+                characterCount += line.Count(x => x == character);
+
+            }
+
+            return characterCount;
 
         }
 
