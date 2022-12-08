b0dcfaa63db1f228453bb0e315ea21787b8046a3
DanielTakac <danieltakac@protonmail.com>
2022-12-08 14:36:34 +0100

    Finished first part of day 1 2021

--- 2021/day1/day1.cs
+++ 2021/day1/day1.cs
@@ -10,10 +10,40 @@ namespace AdventOfCode {
 
         static void Main(string[] args) {
 
-            string[] input = AdventOfCode.GetInput(path: "demo.txt");
+            string[] input = AdventOfCode.GetInput();
 
+            int previousMeasurement = 0;
+
+            int part1 = 0;
             
+            foreach (string line in input) {
 
+                int currentMeaserement = int.Parse(line);
+
+                Console.Write(currentMeaserement);
+
+                if (previousMeasurement == 0) {
+
+                    Console.WriteLine(" (N/A - no previous measurement)");
+                    
+                } else if (currentMeaserement > previousMeasurement) {
+
+                    Console.WriteLine(" (increased)");
+
+                    part1++;
+
+                } else {
+
+                    Console.WriteLine(" (decreased)");
+
+                }
+
+                previousMeasurement = currentMeaserement;
+
+            }
+
+            AdventOfCode.PrintWithColor($"\nPart 1: {part1}");
+            
             Console.ReadKey();
 
         }
