2df852fac328dc1dd2a98dc59d267e6e99ada8b5
DanielTakac <danieltakac@protonmail.com>
2022-12-12 08:54:43 +0100

    .

---
+++ 2022/day12/day12.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode {
+
+    class Day12 {
+
+        static void Main(string[] args) {
+
+            string[] input = AdventOfCode.GetInput(path: "demo.txt");
+
+            foreach (string line in input) {
+
+                Console.WriteLine(line);
+
+            }
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
