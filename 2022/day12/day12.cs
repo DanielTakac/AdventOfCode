f60973fe7c35c4cbf358fd0f0f41d8bd8b0d0519
DanielTakac <danieltakac@protonmail.com>
2022-12-12 23:43:25 +0100

    .

--- 2022/day12/day12.cs
+++ 2022/day12/day12.cs
@@ -16,7 +16,7 @@ namespace AdventOfCode {
 
             Dictionary<int[], bool> visitedPositions = new Dictionary<int[], bool>();
 
-            int GetHeight(char character) {
+            int Height(char character) {
 
                 if (character == 'S') {
 
@@ -32,6 +32,33 @@ namespace AdventOfCode {
 
                 }
                 
+            }
+
+            int[] Neighbors(int x, int y) {
+
+                int currentHeight = Height(input[x][y]);
+
+                int[,] neighbors = new int[,] { { x + 1, y }, { x - 1, y }, { x, y + 1 }, { x, y - 1 } };
+
+                for (int i = 0; i < neighbors.Length; i++) {
+
+                    if (!(neighbors[i, 0] >= 0 && neighbors[i, 0] < input.Length &&
+                          neighbors[i, 1] >= 0 && neighbors[i, 1] < input[0].Length)) {
+
+                        continue;
+
+                    }
+
+                    if (Height(input[neighbors[i, 0]][neighbors[i, 1]]) <= currentHeight) {
+
+                        return new int[] { neighbors[i, 0], neighbors[i, 1] };
+
+                    }
+
+                }
+
+                return null;
+
             }
             
             for (int x = 0; x < input.Length; x++) {
@@ -54,9 +81,30 @@ namespace AdventOfCode {
 
             }
 
-            // Dijkstras Algorithm
+            // Dijkstra's Algorithm
 
-            
+            List<Dictionary<int[], bool>> visited = new List<Dictionary<int[], bool>>();
+
+            for (int i = 0; i < input.Length; i++) {
+
+                for (int j = 0; j < input[i].Length; j++) {
+
+                    visited.Add(new Dictionary<int[], bool>() { new int[] { i, j }, false });
+
+                }
+
+            }
+
+            List<int[]> queue= new List<int[]>() { new int[] { startPos[0], startPos[1], 0 } };
+
+            while (true) {
+
+                
+
+            }
+
+            Console.WriteLine(startPos[0] + " " + startPos[1]);
+            Console.WriteLine(endPos[0] + " " + endPos[1]);
 
             Console.ReadKey();
 
