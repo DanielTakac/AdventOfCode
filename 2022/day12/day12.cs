b6a0a1ce26e2185f766cd0519d00db29a42b56b3
DanielTakac <danieltakac@protonmail.com>
2022-12-13 22:30:41 +0100

    .

--- 2022/day12/day12.cs
+++ 2022/day12/day12.cs
@@ -13,53 +13,6 @@ namespace AdventOfCode {
 
             int[] startPos = new int[2];
             int[] endPos = new int[2];
-
-            Dictionary<int[], bool> visitedPositions = new Dictionary<int[], bool>();
-
-            int Height(char character) {
-
-                if (character == 'S') {
-
-                    return 0;
-                    
-                } else if (character == 'E') {
-
-                    return 25;
-
-                } else {
-
-                    return (int)character - 97;
-
-                }
-                
-            }
-
-            int[] Neighbors(int x, int y) {
-
-                int currentHeight = Height(input[x][y]);
-
-                int[,] neighbors = new int[,] { { x + 1, y }, { x - 1, y }, { x, y + 1 }, { x, y - 1 } };
-
-                for (int i = 0; i < neighbors.Length; i++) {
-
-                    if (!(neighbors[i, 0] >= 0 && neighbors[i, 0] < input.Length &&
-                          neighbors[i, 1] >= 0 && neighbors[i, 1] < input[0].Length)) {
-
-                        continue;
-
-                    }
-
-                    if (Height(input[neighbors[i, 0]][neighbors[i, 1]]) <= currentHeight) {
-
-                        return new int[] { neighbors[i, 0], neighbors[i, 1] };
-
-                    }
-
-                }
-
-                return null;
-
-            }
             
             for (int x = 0; x < input.Length; x++) {
 
@@ -89,17 +42,42 @@ namespace AdventOfCode {
 
                 for (int j = 0; j < input[i].Length; j++) {
 
-                    nodes.Add(new Node(new int[] { i, j }));
+                    nodes.Add(new Node(i, j));
 
                 }
 
             }
 
-            var queue = new List<Node>() { new Node(new int[] { 0, 0 }, 0) };
+            AdventOfCode.PrintWithColor(nodes.Count.ToString());
+
+            var queue = new List<Node>() { new Node(0, 0, 0) };
 
             while (true) {
 
-                
+                Node node = queue.OrderBy(n => n.Distance).First();
+
+                Node newNode = new Node(node.X, node.Y, node.Distance);
+
+                queue.Remove(node);
+
+                if (newNode.Visited) continue;
+
+                newNode.Visited = true;
+
+                if (newNode.X == endPos[0] && newNode.Y == endPos[1]) {
+
+                    AdventOfCode.PrintWithColor(newNode.Distance.ToString());
+                    break;
+
+                }
+
+                foreach (var neighbor in newNode.Neighbors(new List<Node>())) {
+
+                    neighbor.Distance++;
+
+                    queue.Add(neighbor);
+
+                }
 
             }
 
@@ -112,30 +90,113 @@ namespace AdventOfCode {
 
         class Node {
 
-            public int[] Position { get; set; }
+            public int X { get; set; }
+            public int Y { get; set; }
             public int Distance { get; set; }
+            public int Height { get; set; }
             public bool Visited { get; set; }
 
-            public Node(int[] position) {
+            private Node() {
 
-                Position = position;
+                Height = GetHeight();
                 Visited = false;
 
             }
 
-            public Node(int[] position, int distance) {
+            public Node(int x, int y) : this() {
 
-                Position = position;
+                X = x;
+                Y = y;
+
+            }
+
+            public Node(int x, int y, int distance) : this() {
+
+                X = x;
+                Y = y;
+                
                 Distance = distance;
-                Visited = false;
 
             }
 
-            public Node(int[] position, int distance, bool visited) {
+            public Node(int x, int y, int distance, bool visited) : this() {
 
-                Position = position;
+                X = x;
+                Y = y;
+                
                 Distance = distance;
                 Visited = visited;
+
+            }
+
+            private int GetHeight() {
+
+                char character = AdventOfCode.GetInput(path: "demo.txt")[X][Y];
+
+                if (character == 'S') {
+
+                    return 0;
+
+                } else if (character == 'E') {
+                    
+                    return 25;
+
+                } else {
+
+                    return (int)character - 97;
+
+                }
+
+            }
+
+            public List<Node> Neighbors(List<Node> neighbors) {
+
+                string[] input = AdventOfCode.GetInput(path: "demo.txt");
+
+                Node[] potentialNeighbors = new Node[] {
+                    new Node(X + 1, Y),
+                    new Node(X - 1, Y),
+                    new Node(X, Y + 1),
+                    new Node(X, Y - 1)
+                };
+
+                bool NeighborAlreadyExists(int i) {
+
+                    for (int j = 0; j < neighbors.Count; j++) {
+
+                        if (neighbors[j].X == potentialNeighbors[i].X &&
+                            neighbors[j].Y == potentialNeighbors[j].Y) {
+
+                            return true;
+
+                        }
+
+                    }
+
+                    return false;
+
+                }
+
+                for (int i = 0; i < potentialNeighbors.Length; i++) {
+
+                    if (NeighborAlreadyExists(i)) continue;
+
+                    if (!(potentialNeighbors[i].X >= 0 && potentialNeighbors[i].X < input.Length &&
+                          potentialNeighbors[i].Y >= 0 && potentialNeighbors[i].Y < input[0].Length)) {
+
+                        continue;
+
+                    }
+
+                    if (potentialNeighbors[i].Height <= this.Height) {
+
+                        neighbors.Add(potentialNeighbors[i]);
+
+                    }
+
+                }
+
+                return neighbors;
 
             }
 
