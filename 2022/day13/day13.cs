d41e5a5e87c0537a7f8a7b0e9b376c1003cefb5a
DanielTakac <danieltakac@protonmail.com>
2022-12-17 00:05:17 +0100

    Finished day 13

--- 2022/day13/day13.cs
+++ 2022/day13/day13.cs
@@ -31,7 +31,32 @@ namespace AdventOfCode {
 
             }
 
+            // Part 2
+
+            var firstDividerPacket = new List<object>() { new List<object>() { new List<object>() { 2 } } };
+            var secondDividerPacket = new List<object>() { new List<object>() { new List<object>() { 6 } } };
+
+            List<List<object>> packets = new List<List<object>>() { firstDividerPacket, secondDividerPacket };
+            
+            for (int i = 0; i < input.Length; i++) {
+
+                if (input[i] == string.Empty) continue;
+
+                packets.Add(ListParser.ParseList(ListParser.StringToQueue(input[i].Trim())));
+
+            }
+
+            Comparison<List<object>> comparison = (x, y) => Packets.CompareElements(x, y);
+
+            packets.Sort(comparison);
+
+            int indexOfFirstDividerPacket = packets.IndexOf(firstDividerPacket) + 1;
+            int indexOfSecondDividerPacket = packets.IndexOf(secondDividerPacket) + 1;
+
+            int part2 = indexOfFirstDividerPacket * indexOfSecondDividerPacket;
+
             AdventOfCode.PrintWithColor($"Part 1: {part1}");
+            AdventOfCode.PrintWithColor($"Part 2: {part2}");
 
             Console.ReadKey();
             
