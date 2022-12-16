d3a2bf58c12e4b49a9f4e7e7f6962759b624920e
DanielTakac <danieltakac@protonmail.com>
2022-12-16 09:07:10 +0100

    Added day 13 project

---
+++ 2022/day13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode {
+
+    class Day13 {
+
+        static void Main(string[] args) {
+
+            string[] input = AdventOfCode.GetInput(path: "demo.txt");
+
+            
+
+            Console.ReadKey();
+            
+
+        }
+
+    }
+
+}
