e94319763583913dc917985e988371ddf9a0d1aa
DanielTakac <danieltakac@protonmail.com>
2022-12-20 20:44:23 +0100

    .

---
+++ 2022/day18/Cube.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace day18 {
+    
+    class Cube {
+
+        public int[] Position { get; set; }
+        public bool[] Sides { get; set; }
+
+        public Cube(int[] position) {
+
+            Position = position;
+            Sides = new bool[6];
+
+        }
+
+    }
+
+}
---
+++ 2022/day18/day18.cs
@@ -0,0 +1,32 @@
+﻿using day18;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode {
+
+    class Day13 {
+
+        static void Main(string[] args) {
+
+            string[] input = AdventOfCode.GetInput();
+
+            // 1. Go through the input and add the cubes into a list of a class Cube
+            // 2. Each cube class will have an array of bool sides
+
+            foreach (string line in input) {
+
+                int[] position = Array.ConvertAll(line.Split(","), x => int.Parse(x));
+
+                Cube cube = new Cube(position);
+
+            }
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
