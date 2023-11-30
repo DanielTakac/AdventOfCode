43c9fb40414c01db95dd92e93785b58c4737fe18
DanielTakac <danieltakac@protonmail.com>
2023-11-30 12:47:03 +0100

    Added time measuring of each part in 2023/AdventOfCode/Day.cs

--- 2023/AdventOfCode/Day.cs
+++ 2023/AdventOfCode/Day.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace AdventOfCode {
 
@@ -13,12 +14,25 @@ namespace AdventOfCode {
 
         protected void Solve() {
 
-            Console.WriteLine($"Part 1 - {Part1()}");
-            Console.WriteLine($"Part 2 - {Part2()}");
+            Stopwatch sw = new Stopwatch();
+
+            sw.Start();
+            string part1 = Part1();
+            sw.Stop();
+
+            long part1Time = sw.ElapsedMilliseconds;
+
+            Console.WriteLine($"Part 1 - {part1} - {part1Time}ms");
+
+            sw.Restart();
+            string part2 = Part2();
+            sw.Stop();
+
+            long part2Time = sw.ElapsedMilliseconds;
+
+            Console.WriteLine($"Part 2 - {part2} - {part2Time}ms");
 
             // Use spectre console live table
-
-            // Measure runtime in ms
 
         }
 
