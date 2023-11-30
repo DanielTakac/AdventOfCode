7e3af7f10294a3b6c66634827428100e1fd15f37
DanielTakac <danieltakac@protonmail.com>
2023-11-30 12:14:55 +0100

    Added Day.cs

---
+++ 2023/AdventOfCode/Day.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode {
+
+    public abstract class Day {
+
+        protected abstract string Part1();
+        protected abstract string Part2();
+
+        protected void Solve() {
+
+            Console.WriteLine($"Part 1 - {Part1()}");
+            Console.WriteLine($"Part 2 - {Part2()}");
+
+            // Use spectre console live table
+
+            // Measure runtime in ms
+
+        }
+
+    }
+
+}
--- 2023/day1/day1.cs
+++ 2023/day1/day1.cs
@@ -6,11 +6,25 @@ using System.Threading.Tasks;
 
 namespace AdventOfCode {
 
-    class Day1 {
+    class Day1 : Day {
+
+        protected override string Part1() {
+
+            return "TPWCGNCCG";
+
+        }
+
+        protected override string Part2() {
+
+            return 211805.ToString();
+
+        }
 
         static void Main(string[] args) {
 
-            
+            Day1 day = new Day1();
+
+            day.Solve();
 
             Console.ReadKey();
 
