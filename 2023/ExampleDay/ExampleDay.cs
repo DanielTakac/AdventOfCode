de03ea7d531d6dc8e68168aaca5d57ef5fdbfd13
Daniel Takac <danieltakac@protonmail.com>
2023-12-02 00:19:30 +0100

    Added ExampleDay

---
+++ 2023/ExampleDay/ExampleDay.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode {
+
+    class ExampleDay : Day { // ################################################### Change This ###################################################
+
+        protected override string Part1() {
+
+            return string.Empty;
+
+        }
+
+        protected override string Part2() {
+
+            return string.Empty;
+
+        }
+
+        static void Main(string[] args) {
+
+            var day = new ExampleDay();
+
+            day.Solve();
+
+            Console.ReadKey(true);
+
+        }
+
+    }
+
+}
