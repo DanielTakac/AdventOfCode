79d793a7fe74487f9a7dcca060d118f7e56f1d2b
Daniel Takac <danieltakac@protonmail.com>
2023-12-02 09:50:37 +0100

    .

---
+++ 2023/day2/day2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode {
+
+    class Day2 : Day {
+
+        protected override string Part1() {
+
+            return string.Empty;
+
+        }
+
+        protected override string Part2() {
+
+            return string.Empty;
+
+        }
+
+        static void Main(string[] args) {
+
+            var day = new Day2();
+
+            day.Solve();
+
+            Console.ReadKey(true);
+
+        }
+
+    }
+
+}
