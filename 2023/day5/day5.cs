9ea6d59b4cef83ee7d3d3bfde9bbc2fe4a2aebec
Daniel Takac <danieltakac@protonmail.com>
2023-12-05 23:51:34 +0100

    .

--- 2023/day5/day5.cs
+++ 2023/day5/day5.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -67,8 +68,6 @@ namespace AdventOfCode {
 
                     }
 
-                    Console.WriteLine();
-
                 }
 
                 locations.Add(convertedSeed);
@@ -111,19 +110,25 @@ namespace AdventOfCode {
 
         private long[] GetSeeds(string[] input) {
 
-            long[] seeds = input[0].Split(':')[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => long.Parse(x.Trim())).ToArray();
+            return input[0].Split(':')[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => long.Parse(x.Trim())).ToArray();
 
-            Console.Write("Seeds: ");
+        }
 
-            foreach (long seed in seeds) {
+        private List<long[]> GetSeedRanges(string[] input) {
 
-                Console.Write(seed + " ");
+            long[] seeds = GetSeeds(input);
+
+            List<long[]> seedRanges = new List<long[]>();
+
+            for (int i = 0; i < seeds.Length; i += 2) {
+
+                long[] range = { seeds[i], seeds[i + 1] };
+
+                seedRanges.Add(range);
 
             }
 
-            Console.WriteLine();
-
-            return seeds;
+            return seedRanges;
 
         }
 
@@ -159,7 +164,91 @@ namespace AdventOfCode {
 
         protected override string Part2() {
 
-            return string.Empty;
+            string[] input = AdventOfCode.GetInput();
+
+            if (input == null || input.Length == 0) return string.Empty;
+
+            List<LookupTable> lookUpTables = new List<LookupTable>();
+
+            string[] conversionTypes = { "seed-to-soil", "soil-to-fertilizer", "fertilizer-to-water", "water-to-light", "light-to-temperature", "temperature-to-humidity", "humidity-to-location" };
+
+            foreach (string type in conversionTypes) {
+
+                lookUpTables.Add(new LookupTable(type));
+
+            }
+
+            foreach (LookupTable lookupTable in lookUpTables) {
+
+                List<long[]> map = GetMap(input, lookupTable.conversionType);
+
+                List<long> destRanges = new List<long>();
+                List<long> sourceRanges = new List<long>();
+                List<long> rangeLengths = new List<long>();
+
+                foreach (long[] ranges in map) {
+
+                    destRanges.Add(ranges[0]);
+                    sourceRanges.Add(ranges[1]);
+                    rangeLengths.Add(ranges[2]);
+
+                }
+
+                lookupTable.destRanges = destRanges;
+                lookupTable.sourceRanges = sourceRanges;
+                lookupTable.rangeLengths = rangeLengths;
+
+            }
+
+            long[] seeds = GetSeeds(input);
+
+            List<long> locations = new List<long>();
+
+            List<long[]> seedRanges = GetSeedRanges(input);
+
+            Stopwatch sw = new();
+
+            foreach (long[] seedRange in seedRanges) {
+
+                // Change this to int later so it's faster
+                for (long seed = seedRange[0]; seed < seedRange[0] + seedRange[1]; seed++) {
+
+                    long convertedSeed = seed;
+
+                    foreach (LookupTable table in lookUpTables) {
+
+                        for (int i = 0; i < table.sourceRanges.Count; i++) {
+
+                            if (convertedSeed >= table.sourceRanges[i] && convertedSeed < table.sourceRanges[i] + table.rangeLengths[i]) {
+
+                                convertedSeed += table.destRanges[i] - table.sourceRanges[i];
+                                break;
+
+                            }
+
+                        }
+
+                    }
+
+                    locations.Add(convertedSeed);
+
+                }
+
+            }
+
+            long lowestLocation = locations[0];
+
+            foreach (long location in locations) {
+
+                if (location < lowestLocation) {
+
+                    lowestLocation = location;
+
+                }
+
+            }
+
+            return lowestLocation.ToString();
 
         }
 
@@ -167,7 +256,7 @@ namespace AdventOfCode {
 
             var day = new Day5();
 
-            day.Solve();
+            day.Solve(true);
 
             Console.ReadKey(true);
 
