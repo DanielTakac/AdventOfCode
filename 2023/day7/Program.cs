a0dc3decca271ca796dde8c5de69e34853391e5b
DanielTakac <danieltakac@protonmail.com>
2023-12-07 08:44:47 +0100

    .

--- 2023/day7/Program.cs
+++ 2023/day7/Program.cs
@@ -16,6 +16,134 @@ namespace AdventOfCode {
 
         }
 
+        class Hand {
+
+            public string Cards { get; set; }
+            public string Bid { get; set; }
+            public int Value {  get; set; }
+
+            public Hand(string cards) {
+
+                Cards = cards;
+                Bid = string.Empty;
+                Value = GetValue();
+
+            }
+
+            private bool IsFiveOfAKind() {
+
+                char firstCard = Cards[0];
+
+                foreach (char card in Cards) {
+
+                    if (card != firstCard) {
+
+                        return false;
+
+                    }
+
+                }
+
+                return true;
+
+            }
+
+            private bool IsFourOfAKind() {
+
+                char firstCard = Cards[0];
+                char secondCard = Cards[1];
+
+                int firstCardOccurrences = 0;
+                int secondCardOccurrences = 0;
+
+                foreach (char card in Cards) {
+
+                    if (card == firstCard) {
+
+                        firstCardOccurrences++;
+
+                    }
+
+                    if (card == secondCard) {
+
+                        secondCardOccurrences++;
+
+                    }
+
+                }
+
+                if (firstCardOccurrences == 4 || secondCardOccurrences == 4) {
+                    return true;
+                } else {
+                    return false;
+                }
+
+            }
+
+            private bool IsFullHouse() {
+
+                return true;
+
+            }
+
+            private bool IsThreeOfAKind() {
+
+                return true;
+
+            }
+
+            private bool IsTwoPair() {
+
+                return true;
+
+            }
+
+            private bool IsOnePair() {
+
+                return true;
+
+            }
+
+            private bool IsHighCard() {
+
+                return true;
+
+            }
+
+            public int GetValue() {
+
+                // Five of a kind: AAAAA = 7
+                // Four of a kind: AA8AA = 6
+                // Full house: 23332 = 5
+                // Three of a kind: TTT98 = 4
+                // Two pair: 23432 = 3
+                // One pair: A23A4 = 2
+                // High card: 23456 = 1
+
+                int value = 0;
+
+                if (IsFiveOfAKind()) {
+                    value = 7;
+                } else if (IsFourOfAKind()) {
+                    value = 6;
+                } else if (IsFullHouse()) {
+                    value = 5;
+                } else if (IsThreeOfAKind()) {
+                    value = 4;
+                } else if (IsTwoPair()) {
+                    value = 3;
+                } else if (IsOnePair()) {
+                    value = 2;
+                } else if (IsHighCard()) {
+                    value = 1;
+                }
+
+                return value;
+
+            }
+
+        }
+
         protected override string Part2() {
 
             return string.Empty;
