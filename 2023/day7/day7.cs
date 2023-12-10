c6730671bc34f393388288ad07e70ccd94a75118
Daniel Takac <danieltakac@protonmail.com>
2023-12-10 01:47:30 +0100

    Finished part 1 of day 7

--- 2023/day7/day7.cs
+++ 2023/day7/day7.cs
@@ -10,7 +10,7 @@ namespace AdventOfCode {
 
         protected override string Part1() {
 
-            string[] input = AdventOfCode.GetInput("example.txt");
+            string[] input = AdventOfCode.GetInput("input.txt");
 
             string[] handTypes = {
                 "none",
@@ -23,21 +23,113 @@ namespace AdventOfCode {
                 "Five of a kind - AAAAA",
             };
 
-            Hand testHand = new Hand("XXYXX");
+            List<Hand> hands = new List<Hand>();
 
-            Console.WriteLine(testHand.Cards + ": " + testHand.GetValue() + " " + handTypes[testHand.GetValue()]);
+            foreach (string line in input) {
 
-            return string.Empty;
+                string cards = line.Split()[0];
+                int bid = int.Parse(line.Split()[1]);
+
+                Hand hand = new Hand(cards, bid);
+
+                hands.Add(hand);
+
+                Console.WriteLine(hand.Cards + ": " + hand.Value + " " + handTypes[hand.Value]);
+
+            }
+
+            // Sort hands by value
+            hands = hands.OrderBy(x => x.Value).ToList();
+
+            foreach (Hand hand in hands) {
+
+                Console.WriteLine(hand.Cards);
+
+            }
+
+            Console.WriteLine();
+
+            // Sort hands with the same value
+            for (int i = 1; i < hands.Count; i++) {
+
+                if (hands[i].Value == hands[i - 1].Value) {
+
+                    bool inOrder = CompareHands(hands[i - 1].Cards, hands[i].Cards);
+
+                    if (!inOrder) {
+
+                        // AdventOfCode.PrintWithColor($"Cards {hands[i - 1].Cards} and {hands[i].Cards} not in order, swapping them", ConsoleColor.Magenta);
+
+                        // Swap hands
+                        (hands[i - 1], hands[i]) = (hands[i], hands[i - 1]);
+
+                        // Start from the begginning again
+                        i = 1;
+
+                    }
+
+                }
+
+            }
+
+            // 2nd run because the first hand doesn't get sorted for some reason and I'm too lazy to fix it
+            for (int i = 1; i < hands.Count; i++) {
+
+                if (hands[i].Value == hands[i - 1].Value) {
+
+                    bool inOrder = CompareHands(hands[i - 1].Cards, hands[i].Cards);
+
+                    if (!inOrder) {
+
+                        // AdventOfCode.PrintWithColor($"Cards {hands[i - 1].Cards} and {hands[i].Cards} not in order, swapping them", ConsoleColor.DarkGreen);
+
+                        // Swap hands
+                        (hands[i - 1], hands[i]) = (hands[i], hands[i - 1]);
+
+                        // Start from the begginning again
+                        i = 1;
+
+                    }
+
+                }
+
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < hands.Count; i++) {
+
+                if (i != 0 && hands[i - 1].Value != hands[i].Value) {
+
+                    Console.WriteLine();
+
+                }
+
+                Console.WriteLine(hands[i].Cards);
+
+            }
+
+            int totalWinnings = 0;
+
+            for (int i = 0; i < hands.Count; i++) {
+
+                Console.WriteLine($"{hands[i].Cards} - {hands[i].Bid} * {i + 1} = {hands[i].Bid * (i + 1)}");
+
+                totalWinnings += hands[i].Bid * (i + 1);
+
+            }
+
+            return totalWinnings.ToString();
 
         }
 
         class Hand {
 
-            public string Cards { get; set; }
-            public string Bid { get; set; }
-            public int Value {  get; set; }
+            public string Cards { get; private set; }
+            public int Bid { get; private set; }
+            public int Value {  get; private set; }
 
-            public Hand(string cards) {
+            public Hand(string cards, int bid) {
 
                 if (cards.Length == 5) {
 
@@ -50,86 +142,120 @@ namespace AdventOfCode {
 
                 }
 
-                Bid = string.Empty;
+                Bid = bid;
                 Value = GetValue();
 
             }
 
-            private bool IsFiveOfAKind() {
+            private bool IsFiveOfAKind(Dictionary<char, int> cardCounts) {
 
-                char firstCard = Cards[0];
-
-                foreach (char card in Cards) {
-
-                    if (card != firstCard) {
-
-                        return false;
-
-                    }
-
-                }
+                if (cardCounts.Count != 1) return false;
 
                 return true;
 
             }
 
-            private bool IsFourOfAKind() {
+            private bool IsFourOfAKind(Dictionary<char, int> cardCounts) {
 
-                char firstCard = Cards[0];
-                char secondCard = Cards[1];
+                if (cardCounts.Count != 2) return false;
 
-                int firstCardOccurrences = 0;
-                int secondCardOccurrences = 0;
+                int card1 = cardCounts.ElementAt(0).Value;
+                int card2 = cardCounts.ElementAt(1).Value;
 
-                foreach (char card in Cards) {
+                if (card1 == 4 && card2 == 1 ||
+                    card1 == 1 && card2 == 4) {
 
-                    if (card == firstCard) {
-
-                        firstCardOccurrences++;
-
-                    }
-
-                    if (card == secondCard) {
-
-                        secondCardOccurrences++;
-
-                    }
-
-                }
-
-                if (firstCardOccurrences == 4 || secondCardOccurrences == 4) {
                     return true;
-                } else {
-                    return false;
+
                 }
 
-            }
-
-            private bool IsFullHouse() {
-
-                return true;
+                return false;
 
             }
 
-            private bool IsThreeOfAKind() {
+            private bool IsFullHouse(Dictionary<char, int> cardCounts) {
 
-                return true;
+                if (cardCounts.Count != 2) return false;
+
+                int card1 = cardCounts.ElementAt(0).Value;
+                int card2 = cardCounts.ElementAt(1).Value;
+
+                if (card1 == 3 && card2 == 2 ||
+                    card1 == 2 && card2 == 3) {
+
+                    return true;
+
+                }
+
+                return false;
 
             }
 
-            private bool IsTwoPair() {
+            private bool IsThreeOfAKind(Dictionary<char, int> cardCounts) {
 
-                return true;
+                if (cardCounts.Count != 3) return false;
+
+                int card1 = cardCounts.ElementAt(0).Value;
+                int card2 = cardCounts.ElementAt(1).Value;
+                int card3 = cardCounts.ElementAt(2).Value;
+
+                if (card1 == 3 ||
+                    card2 == 3 ||
+                    card3 == 3) {
+
+                    return true;
+
+                }
+
+                return false;
 
             }
 
-            private bool IsOnePair() {
+            private bool IsTwoPair(Dictionary<char, int> cardCounts) {
 
-                return true;
+                if (cardCounts.Count != 3) return false;
+
+                int card1 = cardCounts.ElementAt(0).Value;
+                int card2 = cardCounts.ElementAt(1).Value;
+                int card3 = cardCounts.ElementAt(2).Value;
+
+                if (card1 == 2 ||
+                    card2 == 2 ||
+                    card3 == 2) {
+
+                    return true;
+
+                }
+
+                return false;
 
             }
 
-            private bool IsHighCard() {
+            private bool IsOnePair(Dictionary<char, int> cardCounts) {
+
+                if (cardCounts.Count != 4) return false;
+
+                int card1 = cardCounts.ElementAt(0).Value;
+                int card2 = cardCounts.ElementAt(1).Value;
+                int card3 = cardCounts.ElementAt(2).Value;
+                int card4 = cardCounts.ElementAt(3).Value;
+
+                if (card1 == 2 ||
+                    card2 == 2 ||
+                    card3 == 2 ||
+                    card4 == 2) {
+
+                    return true;
+
+                }
+
+                return false;
+
+            }
+
+            private bool IsHighCard(Dictionary<char, int> cardCounts) {
+
+                if (cardCounts.Count != 5) return false;
 
                 return true;
 
@@ -145,27 +271,101 @@ namespace AdventOfCode {
                 // One pair: A23A4 = 2
                 // High card: 23456 = 1
 
+                Dictionary<char, int> cardCounts = new Dictionary<char, int>();
+
+                // Fill up cardCounts dictionary
+                foreach (char card in Cards) {
+
+                    if (cardCounts.ContainsKey(card)) {
+
+                        cardCounts[card]++;
+
+                    } else {
+
+                        cardCounts.Add(card, 1);
+
+                    }
+
+                }
+
+                AdventOfCode.PrintWithColor($"------ {Cards} ------", ConsoleColor.Yellow);
+                Console.WriteLine(cardCounts.Count + " unique cards");
+
+                foreach (KeyValuePair<char, int> card in cardCounts) {
+
+                    AdventOfCode.PrintWithColor($"{card.Key} - {card.Value}");
+
+                }
+
+                Console.WriteLine();
+
                 int value = 0;
 
-                if (IsFiveOfAKind()) {
+                if (IsFiveOfAKind(cardCounts)) {
                     value = 7;
-                } else if (IsFourOfAKind()) {
+                } else if (IsFourOfAKind(cardCounts)) {
                     value = 6;
-                } else if (IsFullHouse()) {
+                } else if (IsFullHouse(cardCounts)) {
                     value = 5;
-                } else if (IsThreeOfAKind()) {
+                } else if (IsThreeOfAKind(cardCounts)) {
                     value = 4;
-                } else if (IsTwoPair()) {
+                } else if (IsTwoPair(cardCounts)) {
                     value = 3;
-                } else if (IsOnePair()) {
+                } else if (IsOnePair(cardCounts)) {
                     value = 2;
-                } else if (IsHighCard()) {
+                } else if (IsHighCard(cardCounts)) {
                     value = 1;
                 }
 
                 return value;
 
             }
+
+        }
+
+        private bool CompareHands(string cards1, string cards2) {
+
+            Dictionary<char, int> cardStrengths = new Dictionary<char, int>() {
+                { '2', 1 },
+                { '3', 2 },
+                { '4', 3 },
+                { '5', 4 },
+                { '6', 5 },
+                { '7', 6 },
+                { '8', 7 },
+                { '9', 8 },
+                { 'T', 9 },
+                { 'J', 10 },
+                { 'Q', 11 },
+                { 'K', 12 },
+                { 'A', 13 }
+            };
+
+            for (int i = 0; i < 5; i++) {
+
+                if (cardStrengths[cards1[i]] == cardStrengths[cards2[i]]) {
+
+                    // Console.WriteLine($"{cards1[i]} == {cards2[i]}");
+
+                    continue;
+
+                } else if (cardStrengths[cards1[i]] < cardStrengths[cards2[i]]) {
+
+                    // Console.WriteLine($"{cards1[i]} < {cards2[i]}");
+
+                    return true;
+
+                } else if (cardStrengths[cards1[i]] > cardStrengths[cards2[i]]) {
+
+                    // Console.WriteLine($"{cards1[i]} > {cards2[i]}");
+
+                    return false;
+
+                }
+
+            }
+
+            return true;
 
         }
 
