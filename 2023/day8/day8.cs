a2a5762f1548b97ad1af7160a172b8174c8dc0f9
Daniel Takac <danieltakac@protonmail.com>
2023-12-10 15:40:13 +0100

    Added day 8

---
+++ 2023/day8/day8.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode {
+
+    class Day8 : Day {
+
+        protected override string Part1() {
+
+            string[] input = AdventOfCode.GetInput("example.txt");
+
+            return string.Empty;
+
+        }
+
+        protected override string Part2() {
+
+            return string.Empty;
+
+        }
+
+        static void Main(string[] args) {
+
+            var day = new Day8();
+
+            day.Solve();
+
+            Console.ReadKey(true);
+
+        }
+
+    }
+
+}
