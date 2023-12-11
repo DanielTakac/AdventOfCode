c7614aefcce4ac70d2bed3ae596e397bf78495c0
Daniel Takac <danieltakac@protonmail.com>
2023-12-11 18:32:50 +0100

    Finished day 9

--- 2023/day9/day9.cs
+++ 2023/day9/day9.cs
@@ -18,14 +18,8 @@ namespace AdventOfCode {
 
                 List<List<int>> sequences = [line.Split().Select(int.Parse).ToList()];
 
-                AdventOfCode.PrintWithColor(line);
-
                 // Loop until the last sequence is all zeroes
                 while (sequences.Last().Where(num => num != 0).Count() != 0) {
-
-                    // foreach (int num in sequences.Last()) AdventOfCode.PrintWithColor(num + " ", ConsoleColor.Cyan, false);
-
-                    // Console.WriteLine();
 
                     List<int> lastSeq = sequences.Last();
                     List<int> newSeq = [];
@@ -52,43 +46,9 @@ namespace AdventOfCode {
 
                 }
 
-                Console.WriteLine();
-
-                for (int i = 0; i < sequences.Count; i++) {
-
-                    Console.Write(i + ": ");
-
-                    for (int s = 0; s < 2 * i; s++) {
-
-                        Console.Write(" ");
-
-                    }
-
-                    for (int j = 0; j < sequences[i].Count; j++) {
-
-                        AdventOfCode.PrintWithColor(sequences[i][j], ConsoleColor.Cyan, false);
-
-                        if (sequences[i][j].ToString().Length == 1) {
-                            Console.Write("   ");
-                        } else {
-                            Console.Write("  ");
-                        }
-
-                    }
-
-                    Console.WriteLine();
-
-                }
-
-                Console.WriteLine();
-
-                Console.WriteLine();
-
                 sumOfPredictedValues += sequences[0].Last();
 
             }
-
-            Console.WriteLine();
 
             return sumOfPredictedValues.ToString();
 
@@ -96,7 +56,47 @@ namespace AdventOfCode {
 
         protected override string Part2() {
 
-            return string.Empty;
+            string[] input = AdventOfCode.GetInput();
+
+            int sumOfPredictedValues = 0;
+
+            foreach (string line in input) {
+
+                List<List<int>> sequences = [line.Split().Select(int.Parse).ToList()];
+
+                // Loop until the last sequence is all zeroes
+                while (sequences.Last().Where(num => num != 0).Count() != 0) {
+
+                    List<int> lastSeq = sequences.Last();
+                    List<int> newSeq = [];
+
+                    for (int i = 0; i < lastSeq.Count - 1; i++) {
+
+                        int diff = lastSeq[i + 1] - lastSeq[i];
+
+                        newSeq.Add(diff);
+
+                    }
+
+                    sequences.Add(newSeq);
+
+                }
+
+                sequences.Last().Insert(0, 0);
+
+                for (int i = sequences.Count - 2; i >= 0; i--) {
+
+                    int diff = sequences[i].First() - sequences[i + 1].First();
+
+                    sequences[i].Insert(0, diff);
+
+                }
+
+                sumOfPredictedValues += sequences[0].First();
+
+            }
+
+            return sumOfPredictedValues.ToString();
 
         }
 
