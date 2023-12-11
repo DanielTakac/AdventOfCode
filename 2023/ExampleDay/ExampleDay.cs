b014d8d951afe823b90554ee5587cade49f91def
Daniel Takac <danieltakac@protonmail.com>
2023-12-11 15:21:16 +0100

    Updated ExampleDay.cs

--- 2023/ExampleDay/ExampleDay.cs
+++ 2023/ExampleDay/ExampleDay.cs
@@ -10,6 +10,8 @@ namespace AdventOfCode {
 
         protected override string Part1() {
 
+            string[] input = AdventOfCode.GetInput("example.txt");
+
             return string.Empty;
 
         }
