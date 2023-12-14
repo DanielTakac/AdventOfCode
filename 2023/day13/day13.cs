295fcf2aafe435d98b9290a037d41800107873dc
DanielTakac <danieltakac@protonmail.com>
2023-12-14 13:18:00 +0100

    .

--- 2023/day13/day13.cs
+++ 2023/day13/day13.cs
@@ -12,7 +12,65 @@ namespace AdventOfCode {
 
             string[] input = AdventOfCode.GetInput("example.txt");
 
+            List<string[]> patterns = [];
+
+            List<string> lines = []; 
+
+            foreach (string line in input) {
+
+                if (line.Length == 0) {
+
+                    patterns.Add(lines.ToArray());
+                    lines = [];
+
+                } else {
+
+                    lines.Add(line);
+
+                }
+
+            }
+
+            foreach (string[] pattern in patterns) {
+
+                int[] rows = GetReflectedRows(pattern);
+
+                Console.Write("Reflected rows: ");
+                foreach (int row in rows) AdventOfCode.PrintWithColor(row + " ", ConsoleColor.Cyan, false);
+                Console.WriteLine();
+
+            }
+
             return string.Empty;
+
+        }
+
+        private static int[] GetReflectedRows(string[] pattern) {
+
+            List<int> reflectedRows = [];
+
+            for (int i = 0; i < pattern.Length - 1; i++) {
+
+                bool mirrored = true;
+
+                for (int j = 0; j < pattern[0].Length; j++) {
+
+                    if (pattern[i][j] != pattern[i + 1][j]) {
+
+                        mirrored = false;
+
+                    }
+
+                }
+                if (mirrored) {
+
+                    reflectedRows.Add(i);
+
+                }
+
+            }
+
+            return reflectedRows.ToArray();
 
         }
 
