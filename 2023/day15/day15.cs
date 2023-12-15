0b5539573942599440698aaeff4cb4b6cde21b6d
Daniel Takac <danieltakac@protonmail.com>
2023-12-15 15:16:17 +0100

    Added day 15

---
+++ 2023/day15/day15.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode {
+
+    class Day15 : Day {
+
+        protected override string Part1() {
+
+            string input = AdventOfCode.GetInput("example.txt")[0];
+
+            Console.WriteLine(input + "\n");
+
+
+
+            return string.Empty;
+
+        }
+
+        protected override string Part2() {
+
+            return string.Empty;
+
+        }
+
+        static void Main(string[] args) {
+
+            var day = new Day15();
+
+            day.Solve();
+
+            Console.ReadKey(true);
+
+        }
+
+    }
+
+}
