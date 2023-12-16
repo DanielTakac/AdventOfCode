5ff0bd9844027b01358323a7aaf771a549e3f12a
Daniel Takac <danieltakac@protonmail.com>
2023-12-17 00:36:52 +0100

    Finished day 16 part 1

--- 2023/day16/day16.cs
+++ 2023/day16/day16.cs
@@ -10,9 +10,174 @@ namespace AdventOfCode {
 
         protected override string Part1() {
 
-            string[] input = AdventOfCode.GetInput("example.txt");
+            string[] input = AdventOfCode.GetInput();
 
-            return string.Empty;
+            bool[,] tiles = new bool[input.Length, input[0].Length];
+
+            tiles[0, 0] = true; // Set starting tile to true
+
+            FollowLightBeam(input, tiles, 0, 0, Direction.Right, new List<Beam>());
+
+            int energizedTiles = CountOfEnergizedTiles(tiles);
+
+            return energizedTiles.ToString();
+
+        }
+
+        public enum Direction {
+            Left,
+            Right,
+            Up,
+            Down
+        }
+
+        public struct Beam {
+
+            public int X { get; }
+            public int Y { get; }
+            public Direction Direction { get; }
+
+            public Beam(int x, int y, Direction direction) {
+
+                X = x;
+                Y = y;
+                Direction = direction;
+
+            }
+
+        }
+
+        private static void FollowLightBeam(string[] tiles, bool[,] energizedTiles, int posX, int posY, Direction dir, List<Beam> visitedBeams) {
+
+            // Already visited
+            if (visitedBeams.Any(b => b.X == posX && b.Y == posY && b.Direction == dir)) {
+
+                return;
+
+            }
+
+            visitedBeams.Add(new Beam(posX, posY, dir));
+
+            int[] pos = [posX, posY];
+
+            void RedirectBeam() {
+
+                switch (tiles[pos[0]][pos[1]]) {
+
+                    case '/':
+
+                        switch (dir) {
+
+                            case Direction.Left:
+                                dir = Direction.Down;
+                                break;
+                            case Direction.Right:
+                                dir = Direction.Up;
+                                break;
+                            case Direction.Up:
+                                dir = Direction.Right;
+                                break;
+                            case Direction.Down:
+                                dir = Direction.Left;
+                                break;
+                        }
+
+                        break;
+                    case '\\':
+
+                        switch (dir) {
+
+                            case Direction.Left:
+                                dir = Direction.Up;
+                                break;
+                            case Direction.Right:
+                                dir = Direction.Down;
+                                break;
+                            case Direction.Up:
+                                dir = Direction.Left;
+                                break;
+                            case Direction.Down:
+                                dir = Direction.Right;
+                                break;
+                        }
+
+                        break;
+                    case '|':
+
+                        if (dir == Direction.Left || dir == Direction.Right) {
+
+                            FollowLightBeam(tiles, energizedTiles, pos[0], pos[1], Direction.Up, visitedBeams);
+                            FollowLightBeam(tiles, energizedTiles, pos[0], pos[1], Direction.Down, visitedBeams);
+                            pos = [-1, -1];
+
+                        }
+
+                        break;
+                    case '-':
+
+                        if (dir == Direction.Up || dir == Direction.Down) {
+
+                            FollowLightBeam(tiles, energizedTiles, pos[0], pos[1], Direction.Left, visitedBeams);
+                            FollowLightBeam(tiles, energizedTiles, pos[0], pos[1], Direction.Right, visitedBeams);
+                            pos = [-1, -1];
+
+                        }
+
+                        break;
+
+                }
+
+            }
+
+            RedirectBeam();
+
+            while (pos[0] >= 0 && pos[0] < tiles.Length &&
+                   pos[1] >= 0 && pos[1] < tiles[0].Length) {
+
+                // Move
+                switch (dir) {
+
+                    case Direction.Left:
+                        pos[1]--;
+                        break;
+                    case Direction.Right:
+                        pos[1]++;
+                        break;
+                    case Direction.Up:
+                        pos[0]--;
+                        break;
+                    case Direction.Down:
+                        pos[0]++;
+                        break;
+
+                }
+
+                // Out of bounds
+                if (!(pos[0] >= 0 && pos[0] < tiles.Length && pos[1] >= 0 && pos[1] < tiles[0].Length)) break;
+
+                energizedTiles[pos[0], pos[1]] = true;
+
+                RedirectBeam();
+
+            }
+
+        }
+
+        private static int CountOfEnergizedTiles(bool[,] tiles) {
+
+            int count = 0;
+
+            foreach (var pos in tiles) {
+
+                if (pos) {
+
+                    count++;
+
+                }
+
+            }
+
+            return count;
 
         }
 
