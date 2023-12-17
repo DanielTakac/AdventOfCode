1b84c6ddbe077ffaf7f4115e0564288994689cc0
Daniel Takac <danieltakac@protonmail.com>
2023-12-17 12:06:59 +0100

    Added day 17

---
+++ 2023/day17/day17.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode {
+
+    class Day17 : Day {
+
+        protected override string Part1() {
+
+            string[] input = AdventOfCode.GetInput("example.txt");
+
+            return string.Empty;
+
+        }
+
+        protected override string Part2() {
+
+            return string.Empty;
+
+        }
+
+        static void Main(string[] args) {
+
+            var day = new Day17();
+
+            day.Solve();
+
+            Console.ReadKey(true);
+
+        }
+
+    }
+
+}
