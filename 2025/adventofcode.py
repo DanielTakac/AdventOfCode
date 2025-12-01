03da9fe1f469e29d8a902ec101cf1df7fd6cad07
Daniel Takac <danieltakac@protonmail.com>
2025-12-01 20:12:09 +0100

    Solved day01 2025

--- 2025/adventofcode.py
+++ 2025/adventofcode.py
@@ -1,5 +1,13 @@
-def read_input(day, example=False):
-    if example:
+from enum import Enum
+import colorama
+
+class Input(Enum):
+    Example = True
+    Real = False
+
+
+def read_input(day, InputType):
+    if InputType == Input.Example:
         path = f'input/day{day:02}-example.txt'
     else:
         path = f'input/day{day:02}.txt'
@@ -8,4 +16,21 @@ def read_input(day, example=False):
         # remove \n from each line
         input = [line.rstrip() for line in input]
         return input
-    
+
+def info(*args):
+    print(f"{colorama.Fore.BLUE}INFO:", *args, end=colorama.Style.RESET_ALL + "\n")
+
+def warn(*args):
+    print(f"{colorama.Fore.YELLOW}WARNING:", *args, end=colorama.Style.RESET_ALL + "\n")
+
+def error(*args):
+    print(f"{colorama.Fore.RED}ERROR:", *args, end=colorama.Style.RESET_ALL + "\n")
+
+def answer(*args):
+    print(f"{colorama.Fore.GREEN}ANSWER:", *args, end=colorama.Style.RESET_ALL + "\n")
+
+def cyan(*args):
+    print(f"{colorama.Fore.CYAN}", *args, end=colorama.Style.RESET_ALL + "\n")
+
+def magenta(*args):
+    print(f"{colorama.Fore.MAGENTA}", *args, end=colorama.Style.RESET_ALL + "\n")
--- 2025/day01.py
+++ 2025/day01.py
@@ -1,6 +1,68 @@
-import adventofcode
+import adventofcode as aoc
 
-input = adventofcode.read_input(1, True)
+input = aoc.read_input(day=1, InputType=aoc.Input.Real)
 
-for line in input:
-    print(line)
+def part1(input):
+    dial = 50
+    zero_count = 0
+    for line in input:
+        dir = line[0]
+        val = int(line[1:])
+
+        if dir.upper() not in  ['L', 'R']:
+            aoc.warn("Unexpected direction:", dir)
+        
+        dial = rotate(dial, dir, val)
+
+        aoc.info("Dial is now at:", dial)
+
+        if dial == 0:
+            zero_count += 1
+            aoc.magenta("Dial hit zero!")
+        
+    aoc.answer(dial, zero_count)
+
+def part2(input):
+    dial = 50
+    zero_count = 0
+    for line in input:
+        dir = line[0]
+        val = int(line[1:])
+
+        if dir.upper() not in  ['L', 'R']:
+            aoc.warn("Unexpected direction:", dir)
+        
+        dial, zero_count = rotate(dial, dir, val, True, zero_count)
+
+        aoc.info("Dial is now at:", dial)
+        
+    aoc.answer(dial, zero_count)
+
+def rotate(dial, dir, val, part2=False, zero_count=0):
+    new_dial = dial
+    if dir.upper() == 'R':
+        for i in range(val):
+            if new_dial == 99:
+                new_dial = 0
+            else:
+                new_dial += 1
+            # part 2
+            if part2 and new_dial == 0:
+                zero_count += 1
+                aoc.magenta("Dial hit zero!")
+    elif dir.upper() == 'L':
+        for i in range(val):
+            if new_dial == 0:
+                new_dial = 99
+            else:
+                new_dial -= 1
+            # part 2
+            if part2 and new_dial == 0:
+                zero_count += 1
+                aoc.magenta("Dial hit zero!")
+    else:
+        aoc.warn("Unexpected direction in rotate:", dir)
+    return new_dial, zero_count
+
+part2(input)
+
