b51d1f23b8b5779ff01be75377305216385b3314
Daniel Takac <daniel@danieltakac.net>
2025-12-02 09:03:07 +0100

    Finish 2025 day02 part 1

--- 2025/day02.py
+++ 2025/day02.py
@@ -20,23 +20,35 @@ def part1(input):
     aoc.answer("Sum of invalid IDs:", invalid_id_sum)
 
 def check_id_validity(id):
-    if id[0] == '0':
-        aoc.error("ID has a leading zero:", id)
+    split_index = len(id) // 2
+    first_half = id[:split_index]
+    second_half = id[split_index:]
+    if first_half == second_half:
+        # aoc.magenta("ID is invalid:", id)
         return False
-    digit_count = {}
-    for digit in id:
-        if not digit.isdigit():
-            aoc.error("ID is not all digits:", id)
-            return False
-        if digit in digit_count:
-            digit_count[digit] += 1
-        else:
-            digit_count[digit] = 1
-    for digit, count in digit_count.items():
-        if count != 2:
-            aoc.cyan("ID is valid:", id)
-            return True
-    aoc.magenta("ID is not valid:", id)
-    return False
+    else:
+        # aoc.cyan("ID is valid:", id)
+        return True
+    
+def part2(input):
+    invalid_id_sum = 0
+    if len(input) != 1:
+        aoc.error("Input should be a single line of ranges")
+        return
+    ranges = input[0].split(',')
+    for r in ranges:
+        ids = r.split('-')
+        if len(ids) != 2:
+            aoc.error("Range should have two IDs:", r)
+            return
+        aoc.info("Checking range:", ids[0], "to", ids[1])
+        for id in range(int(ids[0]), int(ids[1]) + 1):
+            if not check_id_validity2(str(id)):
+                invalid_id_sum += id
+    aoc.answer("Sum of invalid IDs:", invalid_id_sum)
 
-part1(input)
+def check_id_validity2(id):
+    pass
+
+# part1(input)
+part2(input)
