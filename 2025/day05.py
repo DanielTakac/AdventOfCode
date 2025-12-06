c3d62845986568395ff00b445b7d5dc1b79537e7
Daniel Takac <danieltakac@protonmail.com>
2025-12-06 19:29:49 +0100

    .

--- 2025/day05.py
+++ 2025/day05.py
@@ -1,7 +1,7 @@
 import adventofcode as aoc
 import colorama
 
-input = aoc.read_input(day=5, InputType=aoc.Input.Real)
+input = aoc.read_input(day=5, InputType=aoc.Input.Example)
 
 def part1(input):
     fresh_available_id_count = 0
@@ -21,18 +21,36 @@ def id_in_range(id, rng):
     return int(start) <= int(id) <= int(end)
 
 def part2(input):
-    unique_fresh_ids = []
+    # 1. convert str input into array of int ranges
     empty_line_index = input.index('')
-    fresh_ranges = input[:empty_line_index]
+    fresh_ranges_str = input[:empty_line_index]
+    fresh_ranges = []
+    for rng_str in fresh_ranges_str:
+        start_str, end_str = rng_str.split('-')
+        fresh_ranges.append([int(start_str), int(end_str)])
+    # 2. find unique id ranges
+    unique_id_ranges = []
     for rng in fresh_ranges:
-        start_str, end_str = rng.split('-')
-        start = int(start_str)
-        end = int(end_str)
-        aoc.info(f"Scanning range {fresh_ranges.index(rng)+1}, length: {end - start}")
-        for id in range(start, end + 1):
-            if id not in unique_fresh_ids:
-                unique_fresh_ids.append(id)
-    aoc.answer("Part2: Unique fresh ID count:", len(unique_fresh_ids))
+        aoc.info(f"Scanning range {fresh_ranges.index(rng)+1}, [{rng[0]}-{rng[1]}], length: {rng[1] - rng[0] + 1}")
+        # check if overlaps with any other already seen range
+        unique = True
+        for seen_range in unique_id_ranges:
+            if check_if_overlap(rng, seen_range):
+                unique = False
+                aoc.yellow(f"Range [{rng[0]}-{rng[1]}] is not unique")
+                
+                break
+        if unique:
+            unique_id_ranges.append(rng)
+            aoc.cyan(f"Found new unique range: [{rng[0]}-{rng[1]}]")
+    # 3. count unique ids from found ranges
+    unique_id_count = 0
+    for rng in unique_id_ranges:
+        unique_id_count += (rng[1] - rng[0] + 1)
+    aoc.answer("Part2: Unique fresh ID count:", unique_id_count)
+
+def check_if_overlap(rng1, rng2):
+    return max(rng1[0], rng2[0]) <= min(rng1[1], rng2[1])
 
 # part1(input)
 part2(input)
