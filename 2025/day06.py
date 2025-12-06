5edce010c3800a9c3c5de480f2172dcf12f2074a
Daniel Takac <danieltakac@protonmail.com>
2025-12-06 20:32:37 +0100

    .

---
+++ 2025/day06.py
@@ -0,0 +1,9 @@
+import adventofcode as aoc
+
+input = aoc.read_input(day=6, InputType=aoc.Input.Example)
+
+def part1(input):
+    # find blank columns
+    for x in range(len(input[0]))
+
+part1(input)
