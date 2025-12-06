27f932e53b7cfe0d30a2588944efa95a75e96699
Daniel Takac <danieltakac@protonmail.com>
2025-12-06 18:08:46 +0100

    .

--- 2025/day05.py
+++ 2025/day05.py
@@ -14,10 +14,25 @@ def part1(input):
                 fresh_available_id_count += 1
                 print(f"{colorama.Fore.CYAN}Ingredient ID {id} found in fresh range {rng}{colorama.Style.RESET_ALL}")
                 break
-    aoc.answer("Part1: Fresh and available ID count: ", fresh_available_id_count)
+    aoc.answer("Part1: Fresh and available ID count:", fresh_available_id_count)
 
 def id_in_range(id, rng):
     start, end = rng.split('-')
     return int(start) <= int(id) <= int(end)
 
-part1(input)
+def part2(input):
+    unique_fresh_ids = []
+    empty_line_index = input.index('')
+    fresh_ranges = input[:empty_line_index]
+    for rng in fresh_ranges:
+        start_str, end_str = rng.split('-')
+        start = int(start_str)
+        end = int(end_str)
+        aoc.info(f"Scanning range {fresh_ranges.index(rng)+1}, length: {end - start}")
+        for id in range(start, end + 1):
+            if id not in unique_fresh_ids:
+                unique_fresh_ids.append(id)
+    aoc.answer("Part2: Unique fresh ID count:", len(unique_fresh_ids))
+
+# part1(input)
+part2(input)
