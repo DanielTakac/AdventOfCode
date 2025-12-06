805417b30b77a1c1487951bc3727dd24ced46bd1
Daniel Takac <danieltakac@protonmail.com>
2025-12-06 17:55:01 +0100

    Solved day5 part1 2025

--- 2025/day05.py
+++ 2025/day05.py
@@ -1,13 +1,23 @@
 import adventofcode as aoc
+import colorama
 
-input = aoc.read_input(day=5, InputType=aoc.Input.Example)
+input = aoc.read_input(day=5, InputType=aoc.Input.Real)
 
 def part1(input):
-    fresh_ids = []
-    
+    fresh_available_id_count = 0
+    empty_line_index = input.index('')
+    fresh_ranges = input[:empty_line_index]
+    available_ids = input[empty_line_index+1:]
+    for id in available_ids:
+        for rng in fresh_ranges:
+            if id_in_range(id, rng):
+                fresh_available_id_count += 1
+                print(f"{colorama.Fore.CYAN}Ingredient ID {id} found in fresh range {rng}{colorama.Style.RESET_ALL}")
+                break
+    aoc.answer("Part1: Fresh and available ID count: ", fresh_available_id_count)
 
-def in_range(id, range):
-    start, end = range.split('-')
+def id_in_range(id, rng):
+    start, end = rng.split('-')
     return int(start) <= int(id) <= int(end)
 
 part1(input)
