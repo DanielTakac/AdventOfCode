dd7b84bc8b4b8dd95f3d81fa7d1f0f5d7735eefe
Daniel Takac <danieltakac@protonmail.com>
2025-12-06 15:59:01 +0100

    .

---
+++ 2025/day05.py
@@ -0,0 +1,13 @@
+import adventofcode as aoc
+
+input = aoc.read_input(day=5, InputType=aoc.Input.Example)
+
+def part1(input):
+    fresh_ids = []
+    
+
+def in_range(id, range):
+    start, end = range.split('-')
+    return int(start) <= int(id) <= int(end)
+
+part1(input)
